7b5eca7ad705579a37f409340ff490843f8ffefc
Dafantastic004 <144632188+Dafantastic004@users.noreply.github.com>
2024-03-20 08:15:23 +0100

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
+
 using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.WriteLine("Enter your user name: ");
+        string user_name = Console.ReadLine();
+        return user_name;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string user_name = GetUserName();
+        
+    }
+}
+
+using System;
+
+class Program
+{
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.WriteLine("Enter your user name: ");
+        string user_name = Console.ReadLine();
+        return user_name;
+    }
+    static void Main(string[] args)
+    {
+        DisplayWelcome();
+        string user_name = GetUserName();
+        
     }
 }
