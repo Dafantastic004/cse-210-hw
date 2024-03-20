99e7d777883e6f89f9ff2460483134ee59ff2ba1
Dafantastic004 <144632188+Dafantastic004@users.noreply.github.com>
2024-03-20 08:25:45 +0100

    Update Program.cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,15 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment DestinyMath = new MathAssignment("Momoh Destiny", "Math 215","Section 7","Problem 7-21");
+        WrittingAssignment JakeWritting = new WrittingAssignment("Momoh Detiny", "ENG 105", "The Pros and Cons of Tobacco");
+        Console.WriteLine(JakeMath.getSummary());
+        Console.WriteLine(JakeMath.getMathHomeWork());
+        Console.WriteLine(JakeWritting.getSummary());
+        Console.WriteLine(JakeWritting.getWrittingInformation());
     }
 }
